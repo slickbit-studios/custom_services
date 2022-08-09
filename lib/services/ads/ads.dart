ea7637692d9591fc493d75f49f952a08ef6ea2cd
Daniel <daniel.hoogen@web.de>
2022-08-09 21:08:23 +0200

    added ad service

---
+++ lib/services/ads/ads.dart
@@ -0,0 +1,148 @@
+import 'package:custom_services/util/logger.dart';
+import 'package:google_mobile_ads/google_mobile_ads.dart';
+
+class AdService {
+  final String? interstitialAdId;
+  final String? bannerAdId;
+
+  final int interstitialFrequency;
+  final int bannerFrequency;
+
+  int? _lastInterstitial;
+  int? _lastBanner;
+
+  InterstitialAd? _interstitialAd;
+  Ad? _bannerAd;
+
+  AdService({
+    this.interstitialAdId,
+    this.bannerAdId,
+    this.interstitialFrequency = 3000,
+    this.bannerFrequency = 3000,
+  }) {
+    MobileAds.instance.initialize();
+  }
+
+  void requestConsent() {
+    final params = ConsentRequestParameters();
+    ConsentInformation.instance.requestConsentInfoUpdate(
+      params,
+      () async {
+        if (await ConsentInformation.instance.isConsentFormAvailable()) {
+          _loadConsentForm();
+        }
+      },
+      (FormError error) {
+        // Handle the error
+      },
+    );
+  }
+
+  void _loadConsentForm() {
+    ConsentForm.loadConsentForm(
+      (ConsentForm consentForm) async {
+        var status = await ConsentInformation.instance.getConsentStatus();
+        if (status == ConsentStatus.required) {
+          consentForm.show((FormError? formError) => _loadConsentForm());
+        }
+      },
+      (FormError formError) {
+        Logger.instance.error(
+          message: 'Failed to load ad consent form: ${formError.message}',
+        );
+      },
+    );
+  }
+
+  void loadBannerAd({AdSize size = AdSize.banner}) {
+    assert(bannerAdId != null, 'Requested banner ad but no ad id is set');
+
+    var logger = Logger.instance;
+
+    var bannerAd = BannerAd(
+      adUnitId: bannerAdId!,
+      size: size,
+      request: const AdRequest(),
+      listener: BannerAdListener(
+        onAdLoaded: (ad) => _bannerAd = ad,
+        onAdClosed: (ad) => _bannerAd = null,
+        onAdClicked: (ad) => logger.warning(message: 'Banner ad tapped'),
+        onAdFailedToLoad: (ad, error) =>
+            logger.warning(message: 'Ad failed to load ${error.message}'),
+      ),
+    );
+
+    bannerAd.load();
+  }
+
+  Ad? getBannerAd() {
+    int now = DateTime.now().millisecondsSinceEpoch;
+
+    if (_lastBanner != null && _lastBanner! + bannerFrequency > now) {
+      return null;
+    }
+
+    var ad = _bannerAd;
+    _bannerAd = null;
+
+    _lastBanner = now;
+    return ad;
+  }
+
+  void loadInterstitialAd() {
+    assert(
+      interstitialAdId != null,
+      'Requested interstitial but missing ad id',
+    );
+
+    var logger = Logger.instance;
+
+    InterstitialAd.load(
+      adUnitId: interstitialAdId!,
+      request: const AdRequest(),
+      adLoadCallback: InterstitialAdLoadCallback(
+        onAdLoaded: (ad) {
+          _interstitialAd = ad;
+          _interstitialAd?.fullScreenContentCallback =
+              FullScreenContentCallback(
+            onAdDismissedFullScreenContent: (ad) => _bannerAd = null,
+            onAdClicked: (ad) => logger.warning(message: 'Banner ad tapped'),
+          );
+        },
+        onAdFailedToLoad: (error) =>
+            logger.warning(message: 'Ad failed to load ${error.message}'),
+      ),
+    );
+  }
+
+  Ad? getInterstitialAd() {
+    int now = DateTime.now().millisecondsSinceEpoch;
+
+    if (_interstitialAd != null &&
+        _lastInterstitial! + interstitialFrequency > now) {
+      return null;
+    }
+
+    var ad = _interstitialAd;
+    _interstitialAd = null;
+
+    _lastInterstitial = now;
+    return ad;
+  }
+}
+
+class IosTestAdService extends AdService {
+  IosTestAdService()
+      : super(
+          bannerAdId: 'ca-app-pub-3940256099942544/2934735716',
+          interstitialAdId: 'ca-app-pub-3940256099942544/4411468910',
+        );
+}
+
+class AndroidTestAdService extends AdService {
+  AndroidTestAdService()
+      : super(
+          bannerAdId: 'ca-app-pub-3940256099942544/6300978111',
+          interstitialAdId: 'ca-app-pub-3940256099942544/1033173712',
+        );
+}
