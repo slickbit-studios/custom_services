9eaa6a2e042e977d2fafb789a3a9a6de8f89579f
Daniel <daniel.hoogen@web.de>
2022-11-03 08:49:00 +0100

    no usage of firebase analytics and messaging in interfaces

--- lib/services/notifications/notification_handler.dart
+++ lib/services/notifications/notification_handler.dart
@@ -1,20 +1,22 @@
 import 'package:custom_services/services/notifications/message.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
 
-class CloudMessaging {
-  void onBackgroundMessage(Future<void> Function(Message message) onMessage) {
+class NotificationHandler {
+  static void onBackgroundMessage(
+      Future<void> Function(Message message) onMessage) {
     FirebaseMessaging.onBackgroundMessage(
       (message) => onMessage(_transformMessage(message)),
     );
   }
 
-  void onForegroundMessage(Future<void> Function(Message message) onMessage) {
+  static void onForegroundMessage(
+      Future<void> Function(Message message) onMessage) {
     FirebaseMessaging.onMessage.listen(
       (message) => onMessage(_transformMessage(message)),
     );
   }
 
-  Message _transformMessage(RemoteMessage message) {
+  static Message _transformMessage(RemoteMessage message) {
     return Message(
       id: message.messageId,
       title: message.notification?.title,
